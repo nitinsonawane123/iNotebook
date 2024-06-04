27200a2efe1a90d08bd323fda73c1c784d2e7e4b
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-06-04 17:45:14 +0530

    Video 49 Completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Hello$Welcome';
 
 // Create a User using: POST "/api/auth/createuser". No login required
 router.post('/createuser',[
@@ -16,20 +20,32 @@ router.post('/createuser',[
    }
    // Check whether the user with this email exists already
    try {
-
-   
-
    let user = await User.findOne({email: req.body.email});
    if(user){
     return res.status(400).json({error: "Sorry a user with this email already exists"})
    }
+
+   const salt = await bcrypt.genSalt(10);
+   const secPass = await bcrypt.hash(req.body.password, salt);
+
+   // Create a new user
     user = await User.create({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
-   })
-    res.json(user)
-} catch(error){
+    password: secPass,
+   });
+    
+    const data ={
+     user:{
+        id: user.id
+     }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+
+    //res.json(user)
+    res.json({authtoken})
+
+  } catch(error){
     console.error(error.message);
     res.status(500).send("Some Error occurd");
 }
