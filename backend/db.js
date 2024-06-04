f02c57e7309a8ffb2d65955bee97c1beffcfe658
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-06-04 11:59:03 +0530

    Video 44 Completed

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const mongooseURI = 'mongodb://127.0.0.1:27017/inotebookdb';
  
 function connectToMongo() {
     mongoose.connect(mongooseURI).
-        then((data) => { console.log(`connected successfully `) })
+        then((data) => { console.log(`Connected Successfully `) })
         .catch((err) => { console.log(err) })
 }
 module.exports = connectToMongo
