b47d5c26fea2227e7b1277deb7eff1bdbc47bea1
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-06-05 11:14:36 +0530

    Video 51 Completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/fetchuser');
 
 const JWT_SECRET = 'Hello$Welcome';
 
-// Create a User using: POST "/api/auth/createuser". No login required
+// ROUTE:1 Create a User using: POST "/api/auth/createuser". No login required
 router.post('/createuser',[
    body('name', 'Enter a valid name').isLength({ min: 3}),
    body('email', 'Enter a valid email').isEmail(),
@@ -51,7 +52,7 @@ router.post('/createuser',[
 }
 })
 
-// Authenticate a User using: POST "/api/auth/login". No login required
+// ROUTE:2 Authenticate a User using: POST "/api/auth/login". No login required
 router.post('/login',[
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password cannot be blank').exists(),
@@ -89,5 +90,16 @@ router.post('/login',[
  }
  })
 
+// ROUTE:3 Get loggedin User Details using: POST "/api/auth/getuser". Login required
+router.post('/getuser', fetchuser, async (req, res) =>{
 
+ try {
+   userId = req.user.id;
+   const user = await User.findById(userId).select("-password")
+   res.send(user)
+ }catch(error){
+   console.error(error.message);
+   res.status(500).send("Internal Server Error");
+}
+})
 module.exports = router
