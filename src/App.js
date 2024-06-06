53024c4575978069184292d943638b3f3eabc11d
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-06-06 12:33:23 +0530

    Video 59 & 60 Completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import NoteState from './context/notes/NoteState';
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <>
-    <NoteState>
-    <Router>
-    <Navbar/>
-     <Routes>
-    <Route exact path="/" element={<Home/>} />
-    <Route exact path="/about" element={<About/>} />
-    </Routes>
-    </Router>
-    </NoteState>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
     </>
   );
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import NoteContext from "../noteContext";
-import { useState } from "react";
 
 const NoteState = (props) =>{
-    const s1 = {
-        "name": "Test",
-        "class": "5b"
-    }
-    const [state, setState] = useState(s1)
-    const update = () =>{
-        setTimeout(() => {
-          setState({
-            "name": "Testing",
-             "class": "10b"
-          })
-        }, 1000);
-    }
+    const notesInitial = [
+        {
+          "_id": "66600b954ad45fed8e43cfb4",
+          "user": "665f03fcc6a1d572c40409b1",
+          "title": "My Title",
+          "description": "Please walk up early",
+          "tag": "Personal",
+          "date": "2024-06-05T06:54:13.209Z",
+          "__v": 0
+        },
+        {
+          "_id": "66600e4a2c33da373479d3bb",
+          "user": "665f03fcc6a1d572c40409b1",
+          "title": "New Note",
+          "description": "Please access the playlist",
+          "tag": "YouTube",
+          "date": "2024-06-05T07:05:46.669Z",
+          "__v": 0
+        },
+        {
+          "_id": "66603a601a5f831649f7ad66",
+          "user": "665f03fcc6a1d572c40409b1",
+          "title": "New Note Updated",
+          "description": "Please access the playlists",
+          "tag": "YouTube",
+          "date": "2024-06-05T10:13:52.536Z",
+          "__v": 0
+        },
+        {
+          "_id": "66613a25f43d3d8c41d82d6b",
+          "user": "665f03fcc6a1d572c40409b1",
+          "title": "New Note Added",
+          "description": "Please access the playlists",
+          "tag": "YouTube",
+          "date": "2024-06-06T04:25:09.413Z",
+          "__v": 0
+        }
+      ]
+      const [notes, setNotes] = useState(notesInitial)
+  
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
