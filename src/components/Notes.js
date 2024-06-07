83cc8ab76a9de844d7fb2064515c3f115d7a5da1
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-06-07 20:15:02 +0530

    Video 68 Completed

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -46,27 +46,30 @@ const Notes = () => {
       <form className='my-3'>
 <div className="mb-3">
 <label htmlFor="title" className="form-label">Title</label>
-<input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="emailHelp" onChange={onChange}/>
+<input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="emailHelp" onChange={onChange} minLength={5} required/>
 </div>
 <div className="mb-3">
 <label htmlFor="description" className="form-label">Description</label>
-<input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange}/>
+<input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} minLength={5} required/>
 </div>
 <div className="mb-3">
 <label htmlFor="tag" className="form-label">Tag</label>
-<input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange}/>
+<input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange} minLength={5} required/>
 </div>
 </form>
       </div>
       <div className="modal-footer">
         <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
+        <button disabled={note.etitle.length<5 || note.edescription.length<5} onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
       </div>
     </div>
   </div>
 </div>
     <div className="row my-3">
     <h2>Your Note</h2>
+    <div className="container">
+    {notes.length === 0 && 'No Notes to display'}
+    </div>
     {notes.map((note)=>{
       return <Noteitem key={note._id} updateNote={updateNote} note = {note}/>
     })}
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -33,18 +33,8 @@ const NoteState = (props) =>{
             },
             body: JSON.stringify({title, description, tag})
           });
-        const json = await response.json(); 
-        console.log(json)
-          
-        const note =  {
-            "_id": "66613a25f43d3d8c41d82d6b",
-            "user": "665f03fcc6a1d572c40409b1",
-            "title": title,
-            "description": description,
-            "tag": tag,
-            "date": "2024-06-06T04:25:09.413Z",
-            "__v": 0
-          };
+
+        const note = await response.json(); 
         setNotes(notes.concat(note))
        }
 
@@ -62,7 +52,6 @@ const NoteState = (props) =>{
         const json = await response.json(); 
         console.log(json)
         
-        console.log("Deleting the note with id" + id);
         const newNotes = notes.filter((note)=>{return note._id!==id})
         setNotes(newNotes)
       }
