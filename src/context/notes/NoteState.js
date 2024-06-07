9d07aec2860ed634e9121a688b888028c337703f
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-06-07 12:49:30 +0530

    Video 66 Completed

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -48,8 +48,18 @@ const NoteState = (props) =>{
        }
 
       // Delete a Note
-      const deleteNote = (id) => {
-        // TODO : API Call
+      const deleteNote = async (id) => {
+        // API Call
+
+        const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+          method: "DELETE", 
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjY1ZjAzZmNjNmExZDU3MmM0MDQwOWIxIn0sImlhdCI6MTcxNzU2MzI3NX0._IBCbGNcct68fKPaBrnC959L7RyKaTt2NwcKAFboM74"
+          },
+        });
+        const json = await response.json(); 
+        console.log(json)
         
         console.log("Deleting the note with id" + id);
         const newNotes = notes.filter((note)=>{return note._id!==id})
